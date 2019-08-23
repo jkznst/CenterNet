f2f5728bf348f268f53fb3be2ec243d7f26ff103
张鑫 <zhangxin@zhangxindeMacBook-Pro.local>
2019-08-23 19:13:52 +0800

    Update ctdet.py

--- src/lib/datasets/sample/ctdet.py
+++ src/lib/datasets/sample/ctdet.py
@@ -134,7 +134,7 @@ class CTDetDataset(data.Dataset):
         radius = gaussian_radius((math.ceil(h), math.ceil(w)))
         radius = max(0, int(radius))
         radius = self.opt.hm_gauss if self.opt.mse_loss else radius
-        print(h, w, radius)
+        #print(h, w, radius)
         ct = np.array(
           [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2], dtype=np.float32)
         ct_int = ct.astype(np.int32)
