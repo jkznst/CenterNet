11d6e50f89ab8b771b5d41c2f666f98fe9ffa3bb
张鑫 <zhangxin@zhangxindeMacBook-Pro.local>
2019-08-29 21:02:38 +0800

    Update pose_dla_dcn.py

--- src/lib/models/networks/pose_dla_dcn.py
+++ src/lib/models/networks/pose_dla_dcn.py
@@ -469,6 +469,7 @@ class FeatureAdaptation(nn.Module):
     def __init__(self, chi, cho):
         super(FeatureAdaptation, self).__init__()
         self.deformable_groups = 1
+        self.kernel_size = (3, 3)
         self.conv_offset = nn.Conv2d(1,
                                     self.deformable_groups * 2 * self.kernel_size[0] * self.kernel_size[1],
                                     kernel_size=(1, 1),
