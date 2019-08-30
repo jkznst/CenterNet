7322f9dfc9843bb6819429264251f1194d0f66d6
张鑫 <zhangxin@zhangxindeMacBook-Pro.local>
2019-08-30 13:25:52 +0800

    replace in-place sigmoid

--- src/lib/models/utils.py
+++ src/lib/models/utils.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 
 def _sigmoid(x):
-  y = torch.clamp(x.sigmoid_(), min=1e-4, max=1-1e-4)
+  y = torch.clamp(x.sigmoid(), min=1e-4, max=1-1e-4)
   return y
 
 def _gather_feat(feat, ind, mask=None):
