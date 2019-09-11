b0ff5e898fb14770e5809ee6c1a36d34a6478296
张鑫 <zhangxin@zhangxindeMacBook-Pro.local>
2019-09-11 20:58:35 +0800

    Update ctdet.py

--- src/lib/trains/ctdet.py
+++ src/lib/trains/ctdet.py
@@ -14,9 +14,92 @@ from utils.post_process import ctdet_post_process
 from utils.oracle_utils import gen_oracle_map
 from .base_trainer import BaseTrainer
 
+
 class CtdetLoss(torch.nn.Module):
   def __init__(self, opt):
     super(CtdetLoss, self).__init__()
+    self.crit = torch.nn.MSELoss() if opt.mse_loss else FocalLoss()
+    self.crit_centerness = FocalLoss()
+    self.crit_reg = RegL1Loss() if opt.reg_loss == 'l1' else \
+      RegLoss() if opt.reg_loss == 'sl1' else None
+    self.crit_wh = torch.nn.L1Loss(reduction='sum') if opt.dense_wh else \
+      NormRegL1Loss() if opt.norm_wh else \
+        RegWeightedL1Loss() if opt.cat_spec_wh else self.crit_reg
+    self.crit_scale = torch.nn.SmoothL1Loss(size_average=False)
+    self.opt = opt
+
+  def forward(self, outputs, batch):
+    opt = self.opt
+    hm_loss, wh_loss, off_loss, proposal_loss, proposal_scale_loss = 0, 0, 0, 0, 0
+    for s in range(opt.num_stacks):
+      output = outputs[s]
+      if not opt.mse_loss:
+        output['hm'] = _sigmoid(output['hm'])
+
+      if opt.eval_oracle_hm:
+        output['hm'] = batch['hm']
+      if opt.eval_oracle_wh:
+        output['wh'] = torch.from_numpy(gen_oracle_map(
+          batch['wh'].detach().cpu().numpy(),
+          batch['ind'].detach().cpu().numpy(),
+          output['wh'].shape[3], output['wh'].shape[2])).to(opt.device)
+      if opt.eval_oracle_offset:
+        output['reg'] = torch.from_numpy(gen_oracle_map(
+          batch['reg'].detach().cpu().numpy(),
+          batch['ind'].detach().cpu().numpy(),
+          output['reg'].shape[3], output['reg'].shape[2])).to(opt.device)
+
+      hm_loss += self.crit(output['hm'], batch['hm']) / opt.num_stacks
+      if opt.wh_weight > 0:
+        if opt.dense_wh:
+          mask_weight = batch['dense_wh_mask'].sum() + 1e-4
+          wh_loss += (
+                             self.crit_wh(output['wh'] * batch['dense_wh_mask'],
+                                          batch['dense_wh'] * batch['dense_wh_mask']) /
+                             mask_weight) / opt.num_stacks
+        elif opt.cat_spec_wh:
+          wh_loss += self.crit_wh(
+            output['wh'], batch['cat_spec_mask'],
+            batch['ind'], batch['cat_spec_wh']) / opt.num_stacks
+        else:
+          if opt.reg_proposal:
+            out_scale = output['scale'].detach()
+            wh_loss += self.crit_reg(
+              output['wh'] + out_scale, batch['reg_mask'],
+              batch['ind'], batch['wh']) / opt.num_stacks
+          else:
+            wh_loss += self.crit_reg(
+              output['wh'], batch['reg_mask'],
+              batch['ind'], batch['wh']) / opt.num_stacks
+
+      if opt.reg_offset and opt.off_weight > 0:
+        off_loss += self.crit_reg(output['reg'], batch['reg_mask'],
+                                  batch['ind'], batch['reg']) / opt.num_stacks
+
+      if opt.reg_proposal and opt.proposal_weight > 0:
+        output['proposal'] = _sigmoid(output['proposal'])  # for focal loss
+        ignore_mask = batch['proposal'].gt(-1).float()
+        proposal_loss += self.crit_centerness(output['proposal'], batch['proposal'], ignore_mask) / opt.num_stacks
+        ignore_scale_mask = batch['scale'].gt(0).float()
+        valid_num = ignore_scale_mask.sum()
+        proposal_scale_loss += self.crit_scale(output['scale'] * ignore_scale_mask,
+                                               batch['scale']) / valid_num / opt.num_stacks
+
+    loss = opt.hm_weight * hm_loss + opt.wh_weight * wh_loss + \
+           opt.off_weight * off_loss + opt.proposal_weight * proposal_loss + opt.proposal_scale_weight * proposal_scale_loss
+
+    if opt.reg_proposal:
+      loss_stats = {'loss': loss, 'proposal_loss': proposal_loss, 'scale_loss': proposal_scale_loss,
+                    'hm_loss': hm_loss,
+                    'wh_loss': wh_loss, 'off_loss': off_loss}
+    else:
+      loss_stats = {'loss': loss, 'hm_loss': hm_loss,
+                    'wh_loss': wh_loss, 'off_loss': off_loss}
+    return loss, loss_stats
+
+class MSPCtdetLoss(torch.nn.Module):
+  def __init__(self, opt):
+    super(MSPCtdetLoss, self).__init__()
     self.crit = torch.nn.MSELoss() if opt.mse_loss else FocalLoss()
     self.crit_centerness = FocalLoss()
     self.crit_reg = RegL1Loss() if opt.reg_loss == 'l1' else \
@@ -95,7 +178,7 @@ class CtdetLoss(torch.nn.Module):
         ignore_scale_mask = batch['scale'].gt(0).float()
         valid_num = ignore_scale_mask.sum()
         proposal_scale_loss += self.crit_scale(output['scale'] * ignore_scale_mask, batch['scale']) / valid_num / opt.num_stacks
-        
+
     loss = opt.hm_weight * hm_loss + opt.wh_weight * wh_loss + \
            opt.off_weight * off_loss + opt.proposal_weight * proposal_loss + opt.scale_weight * proposal_scale_loss
     if opt.reg_proposal:
@@ -116,7 +199,11 @@ class CtdetTrainer(BaseTrainer):
       loss_states = ['loss', 'proposal_loss', 'scale_loss', 'hm_loss', 'wh_loss', 'off_loss']
     else:
       loss_states = ['loss', 'hm_loss', 'wh_loss', 'off_loss']
-    loss = CtdetLoss(opt)
+
+    if not opt.msp:
+      loss = CtdetLoss(opt)
+    else:
+      loss = MSPCtdetLoss(opt)
     return loss_states, loss
 
   def debug(self, batch, output, iter_id):
